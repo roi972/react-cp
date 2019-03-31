841f1eb8fe2cfbe47ecfccf654c4e267559173e0
roi972 <roi972@gmail.com>
2019-03-31 16:40:11 +0300

    day2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import TodoList from './components/TodoList';
+import AddItem from './components/AddItem';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <AddItem />
+        <hr />
         <TodoList />
       </div>
     );
